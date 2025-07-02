bc561be5a2b61eb41e5ad4a3d0302bcf9d7dddf1
mohitjaint <imohitjaint@gmail.com>
2025-07-02 13:06:52 +0530

    Lecture 03

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,8 +1,14 @@
+import Hello from "./Hello"
 
 function App() {
 
   return (
-    <h1>Hello World !</h1>
+    <>
+      <Hello />
+      <h1>Welcome to React</h1>
+      <p>This is a simple React application.</p>
+    </>
+    
   )
 }
 
