19b642b02db447e8a1c9819e9b3becbdbad1790f
mohitjaint <imohitjaint@gmail.com>
2025-07-03 12:14:35 +0530

    Lecture 05

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,34 @@
+import { useState} from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  let [counter, setCounter]= useState(15)
+  //let counter = 5
+  const addValue = ()=>{
+    counter ++;
+    setCounter(counter);
+    console.log("clicked",counter)
+  }
+
+  const removeValue = ()=>{
+    counter --;
+    setCounter(counter);
+    console.log("clicked",counter)
+  }
+
+  return (
+    <>
+    <h1>Chai aur React</h1>
+    <h2>Counter value: {counter}</h2>
+    <button
+    onClick = {addValue}>Add value</button>
+    <br/>
+    <button 
+    onClick  = {removeValue}>Remove value</button>
+    </>
+  )
+}
+
+export default App
