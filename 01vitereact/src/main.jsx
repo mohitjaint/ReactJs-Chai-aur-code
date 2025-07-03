ec3fde16577222ad4d33375bd37e87e405e32618
mohitjaint <imohitjaint@gmail.com>
2025-07-03 11:09:59 +0530

    Lecture 04

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,9 +1,35 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import { createElement } from 'react'
+
+function MyApp (){
+  return(
+  <div>
+    <h1>This is a custom app</h1>
+    <p>Idk what to write here</p>
+  </div>
+  )
+}
+
+const customElement01 = (
+  <div>
+    <h1>This is a custom element</h1>
+    <p>Idk what to write here</p>
+  </div>
+)
+
+const customElement02 = createElement(
+  'a',
+  {
+    href : "https://google.com",
+    target : "_blank"
+  },
+  'Click me to visit google'
+) 
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+
+    customElement02
+
 )
