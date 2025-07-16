1d5ece1379104ab0e0382d0f85834d87fd272952
mohitjaint <imohitjaint@gmail.com>
2025-07-16 12:06:33 +0530

    Lecture 13 Part - 1

---
+++ 08miniContext/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Login from './Components/Login'
+import UserContextProvider from './context/UserContextProvider'
+import Profile from './Components/Profile'
+
+function App() {
+  return (
+   <UserContextProvider>
+    <Login />
+    <Profile />
+
+   </UserContextProvider>  )
+}
+
+export default App
